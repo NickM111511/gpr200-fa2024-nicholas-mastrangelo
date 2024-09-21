0078f684c70b04abef5efb73937c1c19568eaf24
nicholas mastrangelo <nicholas.mastrangelo@mymail.champlain.edu>
2024-09-21 18:10:12 -0400

    Created a fragment shader and vertex shader files.

--- core/shader.cpp
+++ core/shader.cpp
@@ -8,7 +8,7 @@ void main()
 {
    Color = aColor; //Pass-through
    vec3 pos = aPos;
-   pos.y += sin(uTime + pos.x) / 4.0;	
+   pos.y += abs(sin(uTime * 4 + 8)) / 3.0; // use "pos.x" for assignment 1, use "+ 8" for assignment 2.	
    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0); 
 }
 )";
