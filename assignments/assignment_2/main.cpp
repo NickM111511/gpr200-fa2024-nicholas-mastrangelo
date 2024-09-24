0356f1287abd2a3207cd06c64ca71e7701d9613e
nicholas mastrangelo <nicholas.mastrangelo@mymail.champlain.edu>
2024-09-24 11:21:14 -0400

    fixed the pathing issue

--- assignments/assignment_2/main.cpp
+++ assignments/assignment_2/main.cpp
@@ -5,20 +5,18 @@
 #include <ew/ewMath/ewMath.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <ew/external/stb_image.h>
 
 #include <iostream>
 #include <shader.h>
+
+// #define STB_IMAGE_IMPLEMENTATION
+// #include "stb_image.h"
 
 
 
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
-
-float vertices[] = {
-	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-	 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
-	 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f
-};
 
 int main() {
 	// Initialization of program --- 
@@ -37,17 +35,24 @@ int main() {
 		printf("GLAD Failed to load GL headers");
 		return 1;
 	}
-	// end of window section ---
-	
-	Shader ourShader("Vertexshader.vert","Fragmentshader.frag");
+	// end of window section // start of verticies section ---
+	Shader ourShader("assets/Vertexshader.vert","assets/Fragmentshader.frag");
 
-	//Initialization goes here!
-	unsigned VAO;
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+	 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+	 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+	// end of verticies section // start of buffer section ---
+	unsigned VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
-
-	unsigned int VBO;
 	glGenBuffers(1, &VBO);
-
+	glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
@@ -58,6 +63,62 @@ int main() {
 	// COLOR (RGBA)
 	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(sizeof(float) * 3));
 	glEnableVertexAttribArray(1);
+	// end of buffer section // start of texture section ---
+	unsigned int texture1, texture2;
+
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width, height, nrChannels;
+	/*
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char *data = stbi_load(FileSystem::getPath("Input file here").c_str(), &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_inage_free(data);
+
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	data = stbi_load(FileSystem::getPath("Input file here").c_str(), &width, &height, &nrChammels, 0);
+	if(data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+	ourShader.use();
+	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+	ourShader.setInt("texture2", 1);
+	*/
+	// end of texture section // start of render loop ---
+
+	//glBlendFunc(2 params);
+	//glBlendEquation();
+
 
 	// OVERRIDES THE FIRST glBindBuffer CODE - glBindBuffer(GL_ARRAY_BUFFER, 0);
 	// OR USE glNamedBufferData(VBO, sizeof(vertices), vertices, GL_STATIC_DRAW);
