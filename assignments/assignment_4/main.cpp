a5412c52fdbcf2b2e470c6d1dc00a73ed9e95640
Nicholas Mastrangelo <nicholas.mastrangelo@mymail.champlain.edu>
2024-10-18 10:00:27 -0400

    sending this to station computer

--- assignments/assignment_4/main.cpp
+++ assignments/assignment_4/main.cpp
@@ -42,15 +42,24 @@ int main() {
 	}
 	// end of window section // start of verticies section ---
 	Shader ourShader("assets/Vertexshader.vert","assets/Fragmentshader.frag");
+	Shader transformShader("assets/transform.vert","assets/transform.frag"); // new for assignment 4
 	// Shader backgroundShader("assets/background.vert", "assets/background.frag"); // assignment 2
 
 	float vertices[] = {
+		// positions          // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
+		 0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
+		-0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left 
+	};
+	/*
+	float vertices[] = { // old assignment 2 verticies
 	     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
 		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
 		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
 		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
 	};
-
+	*/
 	unsigned int indices[] = {
 		0, 1, 3,
 		1, 2, 3
